086d9f2d43978f54d36edfa3c2723f3397798325
ixaxaar <root@ixaxaar.in>
2022-12-11 16:11:19 +0530

    clean up

--- app.py
+++ app.py
@@ -7,74 +7,45 @@ from datetime import datetime, date, timedelta
 import sys
 import os
 
-from modules.db import db_connect, fetch_status, add_data_to_db
 from modules.extractor import extract
+from modules.commodities import commodities
 
+log = logging.getLogger(__name__)
 
 
 def dateRange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)+1):
+    for n in range(int((end_date - start_date).days) + 1):
         yield start_date + timedelta(n)
 
-def saveAgMarknetData(driver, md_db, collectionName, collectionValidate, commodity, currentDate):
-    logging.info("Checking fetch status for Date : {}".format(currentDate))
-    status = fetch_status(md_db, collectionValidate, currentDate, commodity)
-    logging.info("Fetch Status for Date : {} :: {}".format(currentDate, status))
-    if status:
-        logging.info('Data for specified Date already in DataBase')
-    else:
-        logging.info('Data Not Present in DB, Start Extraction for Date : {}'.format(currentDate))
-        df = extract(driver, commodity, currentDate)
-        logging.info ('Length of Final DF : {}'.format(len(df)))
-        logging.info (df.head(3))
-        price_records = df.to_dict('records')
-        query = add_data_to_db(md_db, collectionName, price_records)
-        if query == "Done":
-            logging.info('Values Inserted for Commodity : {} :: Date : {}'.format(commodity, currentDate))
 
-def initLogs():
-    cwd = os.getcwd()
-    logPath = cwd+'/logs'
-    if os.path.isdir(logPath) == False:
-        print ('logs Folder not present')
-        os.mkdir(logPath)
-    else :
-        print ('logs Folder present')
-    currentTimeStamp = datetime.now()
-    logFileName = 'logs/logger_' + currentTimeStamp.strftime('%Y_%h_%d_%H_%M') + '.log';
-    logging.basicConfig(filename=logFileName, level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s')
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout)) 
+def saveAgMarknetData(driver, commodity_name, commodity_number, from_date, to_date):
+    df = extract(driver, commodity_name, commodity_number, from_date, to_date)
+    log.info("Length of Final DF : {}".format(len(df)))
+    df.to_csv(f"{commodity_number}.csv")
+
 
 def main():
-    initLogs()
     config = configparser.ConfigParser()
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    try :
-        config.read('config.ini')
-        dbLink = config['DB']['dbLink']
-        dbName = config['DB']['dbName']
+    driver = webdriver.Firefox()
 
-        commodity = input("Enter the commodity: ").capitalize()
-        print ('Commodity : {}'.format(commodity))
-        collectionName = config['Collections'][commodity]
-        collectionValidate = config['Collections'][commodity+'_validate']
+    for commodity in commodities:
+        try:
+            print("Commodity : {}".format(commodity))
 
-        fromDateStr = input('Enter From Date (dd-mm-yyyy): ')
-        toDateStr = input('Enter To Date (Inclusive) (dd-mm-yyyy):')
-        md_db = db_connect(dbLink, dbName)
+            fromDateStr = "01-01-2010"
+            toDateStr = "11-12-2022"
 
-        fromDate = datetime.strptime(fromDateStr, '%d-%m-%Y').date()
-        toDate = datetime.strptime(toDateStr, '%d-%m-%Y').date()
+            fromDate = datetime.strptime(fromDateStr, "%d-%m-%Y").date()
+            toDate = datetime.strptime(toDateStr, "%d-%m-%Y").date()
 
-        for singleDate in dateRange(fromDate, toDate):
-            currentDate = datetime.strftime(singleDate, '%d-%b-%Y')
-            logging.info ('Current Date : {}'.format(currentDate))
-            saveAgMarknetData(driver, md_db, collectionName, collectionValidate, commodity, currentDate)
-    except Exception as error:
-        logging.exception('Exception Thrown in Main App.py')
-    finally:
-        driver.quit()
+            fromDate = datetime.strftime(fromDate, "%d-%b-%Y")
+            toDate = datetime.strftime(toDate, "%d-%b-%Y")
+            saveAgMarknetData(driver, commodity["name"], commodity["value"], fromDate, toDate)
+        except Exception as error:
+            logging.exception("Exception Thrown in Main App.py")
 
-if __name__ == '__main__':
+    driver.quit()
+
+
+if __name__ == "__main__":
     main()
--- modules/extractor.py
+++ modules/extractor.py
@@ -2,6 +2,7 @@ import json
 import time
 import sys
 import pandas as pd
+
 # from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -9,87 +10,69 @@ from selenium.webdriver.support import expected_conditions as EC
 import logging
 from datetime import datetime
 
-PAGE_LINK='https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity={}&Tx_State=0&Tx_District=0&Tx_Market=0&DateFrom={}&DateTo={}&Fr_Date={}&To_Date={}&Tx_Trend=0&Tx_CommodityHead={}&Tx_StateHead=--Select--&Tx_DistrictHead=--Select--&Tx_MarketHead=--Select--'
+log = logging.getLogger(__name__)
 
-COL_RENAME_DICT = {'Min Price (Rs./Quintal)':'min_price', 'Max Price (Rs./Quintal)':'max_price', 'Modal Price (Rs./Quintal)':'modal_price', 'District Name':'district', 'Market Name':'market', 'Commodity':'commodity', 'Variety':'variety', 'Grade':'grade', 'Price Date':'price_date'}
+PAGE_LINK = "https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity={}&Tx_State=0&Tx_District=0&Tx_Market=0&DateFrom={}&DateTo={}&Fr_Date={}&To_Date={}&Tx_Trend=0&Tx_CommodityHead={}&Tx_StateHead=--Select--&Tx_DistrictHead=--Select--&Tx_MarketHead=--Select--"
 
-def commodity_validate(commodity):
-    com = False
-    comm_num = 0
-    comm_name = ''
-    with open('data.json', 'r') as j:
-        commodities = json.loads(j.read())
-    for comm_no in commodities:
-        if commodity.lower() == commodities[comm_no].lower():
-            com = True
-            comm_num = comm_no
-            comm_name = commodities[comm_no]
-    return com, comm_num, comm_name
-
-
-# Function for the next page after 2 pages
-def next_to_second(driver, pageNumber):
-    logging.info("Extracting Page : {}".format(pageNumber))
-    nextElemPath = '//*[@id="cphBody_GridPriceData"]/tbody/tr[52]/td/table/tbody/tr/td[3]/input'
-    df = pd.DataFrame()
-    try:
-        if driver.find_element_by_xpath(nextElemPath):
-            driver.find_element_by_xpath(nextElemPath).click()
-            time.sleep(10)
-            html_next = driver.page_source
-            df_current = pd.read_html(html_next)
-            df_current = pd.DataFrame(df_current[3])
-            logging.info('Data Extracted Process is done for page : {} :: DF Length: {}'.format(pageNumber, len(df_current)))
-            df_next = next_to_second(driver, pageNumber+1)
-            df = df_current.append(df_next, ignore_index=True)
-    except Exception as error:
-        logging.exception ('Exception during page : {} :: {}'.format(pageNumber, error))
-    finally:
-        return df
+COL_RENAME_DICT = {
+    "Min Price (Rs./Quintal)": "min_price",
+    "Max Price (Rs./Quintal)": "max_price",
+    "Modal Price (Rs./Quintal)": "modal_price",
+    "District Name": "district",
+    "Market Name": "market",
+    "Commodity": "commodity",
+    "Variety": "variety",
+    "Grade": "grade",
+    "Price Date": "price_date",
+}
 
 
 def extractFromSecondPage(driver, pageNumber):
-    logging.info("Extracting Page : {}".format(pageNumber))
-    df = pd.DataFrame()
+    log.info("Extracting Page : {}".format(pageNumber))
+    df_current = pd.DataFrame()
     try:
-        nextElemPath = '//*[@id="cphBody_GridPriceData"]/tbody/tr[52]/td/table/tbody/tr/td[1]/input'
+        nextElemPath = "/html/body/form/div[3]/div[6]/div[6]/div[1]/div[2]/div[3]/div/table/tbody/tr[52]/td/table/tbody/tr/td[1]/input"
+
         if driver.find_element_by_xpath(nextElemPath):
             driver.find_element_by_xpath(nextElemPath).click()
             time.sleep(10)
             html = driver.page_source
             df_current = pd.read_html(html)
             df_current = pd.DataFrame(df_current[3])
-            logging.info('Data Extracted Process is done for page : {} :: DF Length: {}'.format(pageNumber, len(df_current)))
-            df_next = next_to_second(driver, pageNumber+1)
-            df = df_current.append(df_next, ignore_index=True)
+
+            nextElemPath = "/html/body/form/div[3]/div[6]/div[6]/div[1]/div[2]/div[3]/div/table/tbody/tr[52]/td/table/tbody/tr/td[3]/input"
+            next = driver.find_element_by_xpath(nextElemPath)
+            while next:
+                next.click()
+                time.sleep(10)
+                html_next = driver.page_source
+                df_next = pd.read_html(html_next)
+                df_next = pd.DataFrame(df_next[3])
+                df_current = df_current.append(df_next, ignore_index=True)
+                next = driver.find_element_by_xpath(nextElemPath)
     except Exception as error:
-        logging.exception ('Exception during page : {} :: {}'.format(pageNumber, error))
+        # log.exception("Exception during page : {} :: {}".format(pageNumber, error))
+        pass
     finally:
-        return df
+        return df_current
 
 
-def extract(driver, commodity, date):
-    logging.info("Start Validation : {} :: {}".format(commodity, date))
-    validation, commodity_number, comm_name = commodity_validate(commodity)
-    logging.info ("Validation Status : {}".format(validation))
+def extract(driver, commodity_name, commodity_number, from_date, to_date):
     pageNumber = 1
     df = pd.DataFrame()
-    if validation:
-        url = PAGE_LINK.format(commodity_number, date, date, date, date, comm_name)
-        driver.get(url)
-        time.sleep(10)
-        html = driver.page_source
-        df_current = pd.read_html(html)
-        df_current = pd.DataFrame(df_current[3])
-        logging.info('Data Extracted Process is done for page : {} :: DF Length: {}'.format(pageNumber, len(df_current)))
-        df_next = extractFromSecondPage(driver, pageNumber+1);
-        df = df_current.append(df_next, ignore_index=True)
-        df.drop(columns=['Sl no.'], inplace=True)
-        df.dropna(inplace=True)
-        df.rename(columns=COL_RENAME_DICT, inplace=True)
-        df['price_date'] = df['price_date'].apply(lambda x: datetime.strptime(x, '%d %b %Y'))
-    else:
-        logging.error("Entered commodity not found! Please Try again....")
-    return df
 
-        
+    url = PAGE_LINK.format(commodity_number, from_date, to_date, from_date, to_date, commodity_name)
+    driver.get(url)
+    # time.sleep(100)
+    html = driver.page_source
+    df_current = pd.read_html(html)
+    df_current = pd.DataFrame(df_current[3])
+
+    df_next = extractFromSecondPage(driver, pageNumber + 1)
+    df = df_current.append(df_next, ignore_index=True)
+    df.drop(columns=["Sl no."], inplace=True)
+    df.dropna(inplace=True)
+    df.rename(columns=COL_RENAME_DICT, inplace=True)
+    df["price_date"] = df["price_date"].apply(lambda x: datetime.strptime(x, "%d %b %Y"))
+
+    return df
